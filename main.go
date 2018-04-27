fa293c2ced4c2dd8ce2ef712a06bb0553a125eca
Konstantin8105 <konstantin8105@gmail.com>
2018-04-27 22:35:09 +0300

    arguments

--- main.go
+++ main.go
@@ -11,27 +11,35 @@ const compiler string = "gcc"
 const logFile string = "/tmp/gcc.log"
 
 func main() {
-	cmd := exec.Command(compiler, os.Args...)
+	var cmd *exec.Cmd
+	if len(os.Args) == 1 {
+		cmd = exec.Command(compiler)
+	} else {
+		cmd = exec.Command(compiler, os.Args[1:]...)
+	}
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", stderr.String())
+		fmt.Fprintf(os.Stdout, "%s", stderr.String())
 		return
 	}
+
+	fmt.Println(stdout.String())
 
 	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		panic(err)
 	}
 
-	defer f.Close()
-
 	if _, err = f.WriteString(fmt.Sprintln("ARG: ", os.Args)); err != nil {
 		panic(err)
 	}
 	if _, err = f.Write(stdout.Bytes()); err != nil {
+		panic(err)
+	}
+	if err = f.Close(); err != nil {
 		panic(err)
 	}
 }
